b81b33019612a9e55423a4cdbb7fa6cda44691aa
Kanhaiya <42319635+KanhaiyaAryal@users.noreply.github.com>
2018-10-03 22:21:32 +0800

    Add files via upload

---
+++ Test/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace LibraryManagement.Tests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(@"C:\chromedriver");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(50));
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
